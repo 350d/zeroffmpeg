36324f4d03c62779a728eadcc9b7f0c3eeb57f70
Vladimir Sobolev <v.sobolev@gmail.com>
2025-05-22 14:30:47 +0300

    xxxx

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -2,28 +2,52 @@
 set -euo pipefail
 
 ###############################################################################
-# Paths & Toolchain
+# Paths
 ###############################################################################
 PREFIX="/usr/local"
 SYSROOT="/usr/xcc/armv6-unknown-linux-gnueabihf/armv6-unknown-linux-gnueabihf/sysroot"
-TOOLCHAIN_PREFIX="armv6-unknown-linux-gnueabihf-"
-
-###############################################################################
-# Export cross compiler tools
-###############################################################################
-export CC="${TOOLCHAIN_PREFIX}gcc"
-export CXX="${TOOLCHAIN_PREFIX}g++"
-export AR="${TOOLCHAIN_PREFIX}ar"
-export LD="${TOOLCHAIN_PREFIX}ld"
-export NM="${TOOLCHAIN_PREFIX}nm"
-export STRIP="${TOOLCHAIN_PREFIX}strip"
-export RANLIB="${TOOLCHAIN_PREFIX}ranlib"
 
 ###############################################################################
 # Ensure pkg-config will look in your ARMHF sysroot
 ###############################################################################
-export PKG_CONFIG_PATH="${SYSROOT}/usr/lib/arm-linux-gnueabihf/pkgconfig"
-export PKG_CONFIG_SYSROOT_DIR="${SYSROOT}"
+export PKG_CONFIG_PATH="$SYSROOT/usr/lib/arm-linux-gnueabihf/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="$SYSROOT"
+
+###############################################################################
+# Download and unpack ARMHF dev packages (including libc) into sysroot
+###############################################################################
+# List of packages to extract
+PKGS=( 
+  libc6-dev:armhf 
+  libv4l-dev:armhf 
+  libdrm-dev:armhf 
+  zlib1g-dev:armhf 
+  libssl-dev:armhf 
+  libx264-dev:armhf 
+  libjpeg-dev:armhf 
+  libpng-dev:armhf 
+)
+
+# Download .deb files
+apt-get update
+apt-get download "${PKGS[@]}"
+
+# Unpack headers, libs and .pc files into sysroot
+mkdir -p "$SYSROOT/usr/include" \
+         "$SYSROOT/usr/lib/arm-linux-gnueabihf/pkgconfig" \
+         "$SYSROOT/usr/lib/arm-linux-gnueabihf"
+for DEB in *.deb; do
+  TMP=$(mktemp -d)
+  dpkg-deb -x "$DEB" "$TMP"
+  # copy include files
+  cp -r "$TMP/usr/include"/* "$SYSROOT/usr/include/" || true
+  # copy libraries
+  cp -r "$TMP/usr/lib/arm-linux-gnueabihf"/* "$SYSROOT/usr/lib/arm-linux-gnueabihf/" || true
+  # copy pkg-config metadata
+  cp "$TMP/usr/lib/arm-linux-gnueabihf/pkgconfig"/*.pc "$SYSROOT/usr/lib/arm-linux-gnueabihf/pkgconfig/" 2>/dev/null || true
+  rm -rf "$TMP"
+done
+rm -f *.deb
 
 ###############################################################################
 # Clone FFmpeg if needed
@@ -34,12 +58,18 @@ fi
 cd ffmpeg
 
 ###############################################################################
-# Configure for cross-build
+# Configure
 ###############################################################################
 ./configure \
   --enable-cross-compile \
-  --cross-prefix="${TOOLCHAIN_PREFIX}" \
-  --cc="$CC" \
+  --cross-prefix=armv6-unknown-linux-gnueabihf- \
+  --cc=armv6-unknown-linux-gnueabihf-gcc \
+  --ar=armv6-unknown-linux-gnueabihf-ar \
+  --as=armv6-unknown-linux-gnueabihf-as \
+  --ld=armv6-unknown-linux-gnueabihf-ld \
+  --nm=armv6-unknown-linux-gnueabihf-nm \
+  --objdump=armv6-unknown-linux-gnueabihf-objdump \
+  --strip=armv6-unknown-linux-gnueabihf-strip \
   --arch=arm --cpu=arm1176jzf-s --target-os=linux \
   --sysroot="$SYSROOT" \
   --prefix="$PREFIX" \
@@ -69,9 +99,10 @@ cd ffmpeg
 ###############################################################################
 # Build & Install
 ###############################################################################
-make -j"$(nproc)"
+make -j$(nproc)
 make install
 
-# Package up the result
-cd "$PREFIX"
-tar czf "../ffmpeg-armv6.tar.gz" *
+###############################################################################
+# Package
+###############################################################################
+tar czf ../ffmpeg-armv6.tar.gz -C "$PREFIX" .
