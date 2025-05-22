25188085503e9760339320a212e680f5a646385d
Vladimir Sobolev <v.sobolev@gmail.com>
2025-05-22 10:01:21 +0300

    test

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -13,12 +13,8 @@ echo "AS=$AS"
 which "$CC" || echo "[$CC not found]"
 which as   || echo "[as not found]"
 
-# Force assembler through gcc
-export AS="$CC"
-
 # Default variables (override by exporting before execution)
 : "${CROSS_TRIPLE:=armv6-unknown-linux-gnueabihf}"
-# Use QEMU_LD_PREFIX if set for accurate sysroot
 : "${SYSROOT:=${QEMU_LD_PREFIX:-/usr/${CROSS_TRIPLE}}}"
 : "${X264_PREFIX:=/usr/local}"
 : "${WORKDIR:=$(pwd)/build}"
@@ -31,20 +27,22 @@ export LD="${CROSS_TRIPLE}-ld"
 export NM="${CROSS_TRIPLE}-nm"
 export RANLIB="${CROSS_TRIPLE}-ranlib"
 export STRIP="${CROSS_TRIPLE}-strip"
+export PKG_CONFIG_PATH="${X264_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH:-}"
 
 # Create build directory
-mkdir -p "$WORKDIR"
+mkdir -p "${WORKDIR}"
 
 # Build x264 dependency
-pushd "$WORKDIR"
+pushd "${WORKDIR}"
   if [ ! -d "x264" ]; then
     git clone --depth 1 https://code.videolan.org/videolan/x264.git
   fi
   cd x264
-  PKG_CONFIG_PATH="${X264_PREFIX}/lib/pkgconfig"
   ./configure \
     --host="${CROSS_TRIPLE}" \
     --cross-prefix="${CROSS_TRIPLE}-" \
+    --cc="${CC}" \
+    --ranlib="${RANLIB}" \
     --prefix="${X264_PREFIX}" \
     --sysroot="${SYSROOT}" \
     --enable-static \
@@ -57,42 +55,48 @@ pushd "$WORKDIR"
   make install
 popd
 
-export PKG_CONFIG_PATH="${X264_PREFIX}/lib/pkgconfig"
+# Clone FFmpeg source
+pushd "${WORKDIR}"
+  if [ ! -d "ffmpeg" ]; then
+    git clone --depth 1 https://git.ffmpeg.org/ffmpeg.git ffmpeg
+  fi
+popd
 
-# Configure FFmpeg for cross-compilation
-./configure \
-  --enable-cross-compile \
-  --cross-prefix="${CROSS_TRIPLE}-" \
-  --host="${CROSS_TRIPLE}" \
-  --arch=arm \
-  --cpu=arm1176jzf-s \
-  --target-os=linux \
-  --sysroot="${SYSROOT}" \
-  --extra-cflags="-I${SYSROOT}/usr/include -I${X264_PREFIX}/include" \
-  --extra-ldflags="-L${SYSROOT}/usr/lib -L${X264_PREFIX}/lib" \
-  --enable-static \
-  --disable-shared \
-  --disable-everything \
-  --enable-protocol=http,https,tls,tcp,udp,file,rtp \
-  --enable-demuxer=rtp,rtsp,h264,mjpeg,image2,image2pipe \
-  --enable-parser=h264,mjpeg \
-  --enable-decoder=h264,mjpeg \
-  --enable-encoder=mjpeg,libx264 \
-  --enable-muxer=mjpeg,mp4,image2,null \
-  --enable-bsf=mjpeg2jpeg \
-  --enable-filter=showinfo,scale,format,colorspace \
-  --enable-indev=lavfi \
-  --enable-libx264 \
-  --enable-openssl \
-  --enable-version3 \
-  --enable-libv4l2 \
-  --enable-libdrm \
-  --enable-zlib \
-  --enable-gpl \
-  --disable-doc \
-  --disable-debug \
-  --prefix="${X264_PREFIX}"
-
-# Build and install FFmpeg
-make -j"$(nproc)"
-make install
+# Configure and build FFmpeg
+pushd "${WORKDIR}/ffmpeg"
+  ./configure \
+    --enable-cross-compile \
+    --cross-prefix="${CROSS_TRIPLE}-" \
+    --cc="${CC}" \
+    --host="${CROSS_TRIPLE}" \
+    --arch=arm \
+    --cpu=arm1176jzf-s \
+    --target-os=linux \
+    --sysroot="${SYSROOT}" \
+    --extra-cflags="-I${SYSROOT}/usr/include -I${X264_PREFIX}/include" \
+    --extra-ldflags="-L${SYSROOT}/usr/lib -L${X264_PREFIX}/lib" \
+    --enable-static \
+    --disable-shared \
+    --disable-everything \
+    --enable-protocol=http,https,tls,tcp,udp,file,rtp \
+    --enable-demuxer=rtp,rtsp,h264,mjpeg,image2,image2pipe \
+    --enable-parser=h264,mjpeg \
+    --enable-decoder=h264,mjpeg \
+    --enable-encoder=mjpeg,libx264 \
+    --enable-muxer=mjpeg,mp4,image2,null \
+    --enable-bsf=mjpeg2jpeg \
+    --enable-filter=showinfo,scale,format,colorspace \
+    --enable-indev=lavfi \
+    --enable-libx264 \
+    --enable-openssl \
+    --enable-version3 \
+    --enable-libv4l2 \
+    --enable-libdrm \
+    --enable-zlib \
+    --enable-gpl \
+    --disable-doc \
+    --disable-debug \
+    --prefix="${X264_PREFIX}"
+  make -j"$(nproc)"
+  make install
+popd
