825f61e7e316ff24139f5dc59a1495251de78fd9
Vladimir Sobolev <v.sobolev@gmail.com>
2025-05-22 11:27:33 +0300

    xxxx

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -1,59 +1,21 @@
 #!/usr/bin/env bash
-set -euxo pipefail
-
-
-#apt-get update
-#apt-get install -y --no-install-recommends \
-#  build-essential \
-#  git \
-#  pkg-config \
-#  yasm \
-#  libssl-dev         \
-#  zlib1g-dev         \
-#  libv4l-dev         \
-#  libdrm-dev         \
-#  libjpeg-dev        \
-#  libpng-dev         \
-#  libx264-dev        \
-#  libavcodec-dev     \
-#  libavformat-dev    \
-#  libavfilter-dev    \
-#  libavutil-dev      \
-#  libswscale-dev     \
-#  libswresample-dev
+set -euo pipefail
 
 SYSROOT="/usr/xcc/armv6-unknown-linux-gnueabihf/armv6-unknown-linux-gnueabihf/sysroot"
-mkdir -p "$SYSROOT/usr/lib/arm-linux-gnueabihf/pkgconfig"
-cp /usr/lib/arm-linux-gnueabihf/pkgconfig/libv4l*.pc "$SYSROOT/usr/lib/arm-linux-gnueabihf/pkgconfig/"  
-
 export PKG_CONFIG_SYSROOT_DIR="$SYSROOT"
-export PKG_CONFIG_PATH="$SYSROOT/usr/lib/arm-linux-gnueabihf/pkgconfig:$SYSROOT/usr/lib/pkgconfig:$SYSROOT/usr/share/pkgconfig"
-
-echo "#### DEBUG LIB4L2 ####"
-pkg-config --debug --modversion libv4l2
-echo "#### DEBUG END ####"
-
+export PKG_CONFIG_LIBDIR="$SYSROOT/usr/lib/arm-linux-gnueabihf/pkgconfig:$SYSROOT/usr/share/pkgconfig"
 
 if [ ! -d ffmpeg ]; then
   git clone --depth 1 https://git.ffmpeg.org/ffmpeg.git ffmpeg
 fi
-cd ffmpeg
+pushd ffmpeg
+PKG_CONFIG_PATH="/usr/lib/arm-linux-gnueabihf/pkgconfig:/usr/lib/pkgconfig" \
 ./configure \
   --enable-cross-compile \
   --cross-prefix=armv6-unknown-linux-gnueabihf- \
   --cc=armv6-unknown-linux-gnueabihf-gcc \
-  --arch=arm \
-  --cpu=arm1176jzf-s \
-  --target-os=linux \
-  --sysroot=/usr/xcc/armv6-unknown-linux-gnueabihf/\
-armv6-unknown-linux-gnueabihf/sysroot \
-  --extra-cflags="-I/usr/xcc/armv6-unknown-linux-gnueabihf/\
-armv6-unknown-linux-gnueabihf/sysroot/usr/include -I/usr/local/include" \
-  --extra-ldflags="-L/usr/xcc/armv6-unknown-linux-gnueabihf/\
-armv6-unknown-linux-gnueabihf/sysroot/usr/lib -L/usr/local/lib" \
-  --enable-static \
-  --disable-shared \
-  --disable-everything \
+  --arch=arm --cpu=arm1176jzf-s --target-os=linux \
+  --sysroot="$SYSROOT" \
   --enable-protocol=http,https,tls,tcp,udp,file,rtp \
   --enable-demuxer=rtp,rtsp,h264,mjpeg,image2,image2pipe \
   --enable-parser=h264,mjpeg \
@@ -70,8 +32,8 @@ armv6-unknown-linux-gnueabihf/sysroot/usr/lib -L/usr/local/lib" \
   --enable-libdrm \
   --enable-zlib \
   --enable-gpl \
-  --disable-doc \
-  --disable-debug \
+  --disable-doc --disable-debug \
   --prefix=/usr/local
 make -j$(nproc)
 make install
+popd
